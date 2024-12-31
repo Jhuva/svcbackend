dcadd3edd06f153bda7b2f0350b4005c5eec8910
jhuva <jdzambranoh@unac.edu.pe>
2024-12-30 19:07:34 -0500

    añadiendo cambios de despliegue

--- src/main/java/com/svcbackend/ServleInitializer.java
+++ src/main/java/com/svcbackend/ServleInitializer.java
@@ -1,14 +1,9 @@
 package com.svcbackend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServleInitializer extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SvcBackendApplication.class, args);
-    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
